859e12e252e54aada6fd1f2ee3d9576fcf91c7e4
songyeongeun <ye3557@naver.com>
2025-04-15 20:57:32 +0900

    feat : Spring Boot 메인 클래스 추가

---
+++ src/main/java/com/goormthonuniv/posts3/PostS3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.goormthonuniv.posts3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostS3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostS3ProjectApplication.class, args);
+	}
+
+}
